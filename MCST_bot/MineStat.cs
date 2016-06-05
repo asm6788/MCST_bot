8e396018ecf15e99e500775b79ba17cb24dcf9bf
asm6788 <asm6788@naver.com>
2016-06-05 18:39:53 +0900

    초기 커밋.

---
+++ MCST_bot/MineStat.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * MineStat.cs - A Minecraft server status checker
+ * Copyright (C) 2014, 2016 Lloyd Dilley
+ * http://www.dilley.me/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+public class MineStat
+{
+    const ushort dataSize = 512; // this will hopefully suffice since the MotD should be <=59 characters
+    const ushort numFields = 6;  // number of values expected from server
+    private string address;
+    private ushort port;
+    private bool serverUp;
+    private string motd;
+    private string version;
+    private string currentPlayers;
+    private string maximumPlayers;
+
+    public MineStat(string address, ushort port)
+    {
+        byte[] rawServerData = new byte[dataSize];
+        string[] serverData;
+
+        SetAddress(address);
+        SetPort(port);
+
+        try
+        {
+            // ToDo: Add timeout
+            TcpClient tcpclient = new TcpClient();
+            tcpclient.Connect(address, port);
+            Stream stream = tcpclient.GetStream();
+            byte[] payload = { 0xFE, 0x01 };
+            stream.Write(payload, 0, payload.Length);
+            stream.Read(rawServerData, 0, dataSize);
+            tcpclient.Close();
+        }
+        catch (Exception)
+        {
+            serverUp = false;
+            return;
+        }
+
+        if (rawServerData == null || rawServerData.Length == 0)
+            serverUp = false;
+        else
+        {
+            serverData = Encoding.Unicode.GetString(rawServerData).Split("\u0000\u0000\u0000".ToCharArray());
+            if (serverData != null && serverData.Length >= numFields)
+            {
+                serverUp = true;
+                SetVersion(serverData[2]);
+                SetMotd(serverData[3]);
+                SetCurrentPlayers(serverData[4]);
+                SetMaximumPlayers(serverData[5]);
+            }
+            else
+                serverUp = false;
+        }
+    }
+
+    public string GetAddress()
+    {
+        return address;
+    }
+
+    public void SetAddress(string address)
+    {
+        this.address = address;
+    }
+
+    public ushort GetPort()
+    {
+        return port;
+    }
+
+    public void SetPort(ushort port)
+    {
+        this.port = port;
+    }
+
+    public string GetMotd()
+    {
+        return motd;
+    }
+
+    public void SetMotd(string motd)
+    {
+        this.motd = motd;
+    }
+
+    public string GetVersion()
+    {
+        return version;
+    }
+
+    public void SetVersion(string version)
+    {
+        this.version = version;
+    }
+
+    public string GetCurrentPlayers()
+    {
+        return currentPlayers;
+    }
+
+    public void SetCurrentPlayers(string currentPlayers)
+    {
+        this.currentPlayers = currentPlayers;
+    }
+
+    public string GetMaximumPlayers()
+    {
+        return maximumPlayers;
+    }
+
+    public void SetMaximumPlayers(string maximumPlayers)
+    {
+        this.maximumPlayers = maximumPlayers;
+    }
+
+    public bool IsServerUp()
+    {
+        return serverUp;
+    }
+}
---
+++ MCST_bot/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MCST_bot
+{
+    class Program
+    {
+        static bool serverdown = false;
+        static void Main(string[] args)
+        {
+          
+            Run().Wait();
+        }
+
+        static async Task Run()
+        {
+            var Bot = new Api("Apikey");
+
+            var me = await Bot.GetMe();
+
+            Console.WriteLine("Hello my name is {0}", me.Username);
+
+            var offset = 0;
+
+            TcpClient MinecraftServer = new TcpClient();
+            MineStat ms = null;
+            while (true)
+            {
+                var updates = await Bot.GetUpdates(offset);
+
+                foreach (var update in updates)
+                {
+                    if (update.Message.Text == "/start")
+                    {
+                        await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+                        await Bot.SendTextMessage(update.Message.Chat.Id, "그냥 URL이나 서버 ip를 입력해주시면 서버 상태를 알려줍니다");
+                    }
+                    else
+                    {
+                        Console.WriteLine(update.Message.Text);
+                        await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+                        await Bot.SendTextMessage(update.Message.Chat.Id, "처리중");
+                        ms = new MineStat(update.Message.Text, 25565);
+                        Console.WriteLine("Minecraft server status of {0} on port {1}:", ms.GetAddress(), ms.GetPort());
+                        if (ms.IsServerUp())
+                        {
+                            await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+                            await Bot.SendTextMessage(update.Message.Chat.Id, "버전:" + ms.GetVersion() + " 접속자수:" + ms.GetCurrentPlayers() + " 최대인원:" + ms.GetMaximumPlayers() + " 메세지:" + ms.GetMotd());
+
+                            Console.WriteLine("Server is online running version {0} with {1} out of {2} players.", ms.GetVersion(), ms.GetCurrentPlayers(), ms.GetMaximumPlayers());
+                            Console.WriteLine("Message of the day: {0}", ms.GetMotd());
+                        }
+                        else
+                        {
+                            await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+                            await Bot.SendTextMessage(update.Message.Chat.Id, "서버다운");
+                        }
+                    }
+                    offset = update.Id + 1;
+                }
+
+            }
+        }
+
+       
+    }
+}
